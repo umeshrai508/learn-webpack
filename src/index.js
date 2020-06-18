6adba6a852e75dce8132d91cdbbb7dbd9a4ca4d6
Umesh Rai <umesrai@gmail.com>
2020-06-18 01:50:09 +0100

    added webpack dev server

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Heading from './components/heading/heading'
 
 const heading = new Heading();
 heading.render();
+
 const btnHello = new HelloWorldButton();
 btnHello.render();
 if(process.env.NODE_ENV=== 'production'){
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -11,6 +11,11 @@ module.exports = {
         publicPath: ''
     },
     mode: 'development',
+    devServer:{
+        contentBase: path.resolve(__dirname, 'dist'),
+        index: 'index.html', 
+        port: '9000'
+    },
     module: {
         rules: [{
             test: /\.(png|jpg)$/,
